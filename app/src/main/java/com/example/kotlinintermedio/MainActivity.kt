8106b49969cceab9cc9e6176cec4edac3d6770fe
bgavilanezb <bigavilanezb00@gmail.com>
2025-07-14 18:16:07 +0200

    Lección 1 de la parte dos de la guía de Kotlin de MoureDev - Enum

---
+++ app/src/main/java/com/example/kotlinintermedio/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.kotlinintermedio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Enum classes
+        enumClasses()
+    }
+
+    enum class Direction(val dir: Int) {
+        NORTH(1),
+        SOUTH(-1),
+        EAST(1),
+        WEST(-1);
+
+        fun description(): String {
+
+            return when(this) {
+                NORTH -> "La dirección es NORTE"
+                SOUTH -> "La dirección es SUR"
+                EAST -> "La dirección es ESTE"
+                WEST -> "La dirección es OESTE"
+            }
+        }
+    }
+
+    // Lección 1 Kotlin Intermedio: Enum Classes
+    private fun enumClasses() {
+
+        // Asignación de valores
+        var userDirection: Direction? = null
+        println("Direction: $userDirection")
+
+        userDirection = Direction.WEST
+        println("Direction: $userDirection")
+
+        userDirection = Direction.NORTH
+        println("Direction: $userDirection")
+
+        // Propiedades por defecto
+        println("Propiedad name: ${userDirection.name}")
+        println("Propiedad ordinal: ${userDirection.ordinal}")
+
+        // Funciones
+        println(userDirection.description())
+
+        // Inicialización
+        println(userDirection.dir)
+    }
+}
